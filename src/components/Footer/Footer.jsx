9d0815bb3e3dc7b990d2bd0488e4f2e6c1925f9e
Razzoy <61683221+Razzoy@users.noreply.github.com>
2025-01-07 14:14:17 +0100

    Update Footer.jsx
    v1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,15 @@
+import style from './Footer.module.scss'
+
 export function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className={style.footerContainer}>
+        <span>
+          <img src="src/assets/icons/chevron.png" />
+        </span>
+        <h3>bageriet</h3>
+        <p>Der er mange udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået forandringer</p>
+      </div>
+    </>
   )
 }
